194b1683f1485262e7830276fe9d3aab0395ec82
natechols <nathaniel.echols@gmail.com>
2017-03-21 11:16:05 -0700

    bug fix

--- pbcore/util/__init__.py
+++ pbcore/util/__init__.py
@@ -29,5 +29,5 @@
 #################################################################################
 
 class h5py_dummy(object):
-    def __getattr__(item):
+    def __getattr__(self, name):
         raise ImportError("The h5py module is required to use this optional feature; you can install it using 'pip install h5py' (requires HDF5 library already installed).")
