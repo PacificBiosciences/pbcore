3afebe77115109d5012b4f80ac23a6a2ffa6f631
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2018-08-14 10:48:02 -0700

    using local wheelhouse

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -10,9 +10,9 @@ PIP="pip --cache-dir=${bamboo_build_working_directory:-$PWD}/.pip"
 
 rm -rf   build
 mkdir -p build/bin build/lib build/include build/share
-$PIP install --user -r requirements.txt
-$PIP install --user -r requirements-dev.txt
-$PIP install --user -e ./    
+$PIP install --no-compile --find-link file:///mnt/software/p/python/wheelhouse/thirdparty --user -r requirements.txt
+$PIP install --no-compile --find-link file:///mnt/software/p/python/wheelhouse/thirdparty --user -r requirements-dev.txt
+$PIP install --no-compile --find-link file:///mnt/software/p/python/wheelhouse/thirdparty --user -e ./
 
 set +e
 make pylint # way too many errors right now
