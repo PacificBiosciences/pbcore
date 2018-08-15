bd9030ddad6a52677d5bc34fd0e4da9137cf13bd
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2018-08-14 18:07:40 -0700

    modify WHEELHOUSE

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -6,13 +6,23 @@ set -ex
 
 export PATH=$PWD/build/bin:$PATH
 export PYTHONUSERBASE=$PWD/build
+
 PIP="pip --cache-dir=${bamboo_build_working_directory:-$PWD}/.pip"
+if [[ -z ${bamboo_repository_branch_name+x} ]]; then
+  WHEELHOUSE=/mnt/software/p/python/wheelhouse/develop
+elif [[ ${bamboo_repository_branch_name} == develop ]]; then
+  WHEELHOUSE=/mnt/software/p/python/wheelhouse/develop
+elif [[ ${bamboo_repository_branch_name} == master ]]; then
+  WHEELHOUSE=/mnt/software/p/python/wheelhouse/master
+else
+  WHEELHOUSE=/mnt/software/p/python/wheelhouse/develop
+fi
 
 rm -rf   build
 mkdir -p build/bin build/lib build/include build/share
-$PIP install --no-compile --find-link file:///mnt/software/p/python/wheelhouse/thirdparty --user -r requirements.txt
-$PIP install --no-compile --find-link file:///mnt/software/p/python/wheelhouse/thirdparty --user -r requirements-dev.txt
-$PIP install --no-compile --find-link file:///mnt/software/p/python/wheelhouse/thirdparty --user -e ./
+$PIP install --no-compile --find-link $WHEELHOUSE --user -r requirements.txt
+$PIP install --no-compile --find-link $WHEELHOUSE --user -r requirements-dev.txt
+$PIP install --no-compile --find-link $WHEELHOUSE --user -e ./
 
 set +e
 make pylint # way too many errors right now
