375ea87d92dba7bacc500c1b867de191786af095
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-06-21 07:28:43 -0700

    pytest for run_split, copied verbatim from falcon3

---
+++ tests/test_dataset_run_split.py
@@ -0,0 +1,22 @@
+import pbcore.io.dataset.run_split as M
+import glob
+import os
+import pytest
+
+@pytest.fixture(scope="module")
+def script_loc(request):
+    '''Return the directory of the currently running test script'''
+
+    # uses .join instead of .dirname so we get a LocalPath object instead of
+    # a string. LocalPath.join calls normpath for us when joining the path
+    return request.fspath.join('..')
+
+def test(tmpdir, script_loc):
+    tmpdir.chdir()
+    dset = script_loc.join('data/test_dataset_run_split/filt.xml')
+    prefix = 'myprefix'
+    M.run_split_dataset(dset, prefix)
+    globbed_fns = glob.glob('myprefix.*.subreadset.xml')
+    assert len(globbed_fns) == 1
+    fns = [os.path.basename(fn.rstrip()) for fn in open('myprefix.fofn').readlines()]
+    assert globbed_fns == fns
